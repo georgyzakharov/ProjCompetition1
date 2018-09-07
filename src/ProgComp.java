e894f39cd1f435cf39d31cda3d01e92806f59bff
ddn12 <navyd@sunyit.edu>
2018-09-07 11:27:26 -0400

    Beginning Framework

---
+++ src/ProgComp.java
@@ -0,0 +1,9 @@
+
+public class ProgComp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/ProgCompTeam.java
+++ src/ProgCompTeam.java
@@ -1,5 +1,21 @@
+import java.io.File;
+import java.lang.Thread;
+import java.lang.Process;
+import java.lang.Runtime;
+import java.util.ArrayList;
 
-public class ProgCompTeam
+public class ProgCompTeam implements Runnable
 {
-
+	public ProgCompTeam(File Directory) 
+	{
+		
+	}
+	
+	public void run() 
+	{
+		
+	}
+	
+	private File Directory;
+	private ArrayList<File> Files;
 }
