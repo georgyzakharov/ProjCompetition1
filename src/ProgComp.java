56ddd54baa3df718b4dfbed8f639d38c0fbb488f
ddn12 <navyd@sunyit.edu>
2018-09-11 15:22:15 -0400

    Changed a 1 to a 0

--- src/ProgComp.java
+++ src/ProgComp.java
@@ -14,7 +14,7 @@ public class ProgComp
 		ProgCompTeam test = new ProgCompTeam(new File("/home/students/test"));
 		
 		threads.add(new Thread(test));
-		threads.get(1).start();
+		threads.get(0).start();;
 
 	}
 	
