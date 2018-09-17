622f1ae63f52a228dd0ffc117486b84888361a7d
ddn12 <navyd@sunyit.edu>
2018-09-16 21:22:32 -0400

    Added a second team to ensure everything works asynchronously

--- src/ProgComp.java
+++ src/ProgComp.java
@@ -11,12 +11,13 @@ public class ProgComp
 		threads = new ArrayList<Thread>();
 		
 		ProgCompTeam test = new ProgCompTeam(new File("/home/students/test"));
-		
+		ProgCompTeam test2 = new ProgCompTeam(new File("/home/students/test2"));
 		
 		threads.add(new Thread(test));
 		threads.get(0).start();
 		
-		
+		threads.add(new Thread(test2));
+		threads.get(1).start();
 		
 		//Waits for all threads to finish before termination
 		while(true)
